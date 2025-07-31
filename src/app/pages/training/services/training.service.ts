d72edca889cceb32c48054e58b8c2f5d8852b237
SayanGBiswas <sayan.biswas2@in.ey.com>
2025-07-31 16:42:08 +0530

    Bulk upload api and excel template download api integrated and validations applied on the excel upload file.

--- src/app/pages/training/services/training.service.ts
+++ src/app/pages/training/services/training.service.ts
@@ -5,14 +5,32 @@ import { environment } from '../../../../environments/environment';
 import { LoginResponse } from '../models/training.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TrainingService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(credentials: any): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(`${this.apiUrl}/auth/login`, credentials);
+    return this.http.post<LoginResponse>(
+      `${this.apiUrl}/auth/login`,
+      credentials
+    );
+  }
+  downloadExcelFile(): Observable<Blob> {
+    const url = `${this.apiUrl}/trainees/download-trainee-template`;
+    return this.http.get(url, {
+      responseType: 'blob',
+    });
+  }
+  uploadTraineeExcel(file: File, trainingId: number): Observable<any> {
+    const url = `${this.apiUrl}/trainees/upload-trainees-excel`;
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('trainingId', trainingId.toString());
+
+    return this.http.post(url, formData);
   }
 }
