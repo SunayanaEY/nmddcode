50261ce31d17f810bdd65ee88740438ba1ad21e6
SayanGBiswas <sayan.biswas2@in.ey.com>
2025-08-04 16:58:51 +0530

    Latest stable version

--- src/app/pages/training/services/training.service.ts
+++ src/app/pages/training/services/training.service.ts
@@ -11,4 +11,24 @@ export class TrainingService {
   private apiUrl = environment.apiUrl + 'api';
 
   constructor(private http: HttpClient) {}
+
+  downloadExcelFile(): Observable<Blob> {
+    const url = `${this.apiUrl}/trainees/download-trainee-template`;
+    return this.http.get(url, {
+      responseType: 'blob',
+    });
+  }
+  uploadTraineeExcel(file: File, trainingId: number): Observable<any> {
+    const url = `${this.apiUrl}/trainees/upload-trainees-excel`;
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('trainingId', trainingId.toString());
+
+    return this.http.post(url, formData);
+  }
+  submitTrainees(participants: any[]): Observable<any> {
+    const url = `${this.apiUrl}/trainees/manual-upload`;
+    return this.http.post(url, participants);
+  }
 }
